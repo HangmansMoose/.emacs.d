4fb789f92c614cf7b7541107aa8b980036e0abcf
HangmansMoose <trivial86@protonmail.com>
2024-10-08 16:52:16 +1100

    got the background of gruvbox to darken

---
+++ elpa/kanagawa-themes-20240910.1649/kanagawa-themes-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "kanagawa-themes" "20240910.1649"
+  "Elegant theme inspired by The Great Wave off Kanagawa."
+  '((emacs "24.3"))
+  :url "https://github.com/Fabiokleis/kanagawa-emacs"
+  :commit "94418acc89f789f4172c5f68b2afacd784bf176d"
+  :revdesc "94418acc89f7"
+  :keywords '("themes" "faces")
+  :authors '(("Meritamen" . "meritamen@sdf.org"))
+  :maintainers '(("Fabio Kleis" . "fabiohkrc@gmail.com")))
